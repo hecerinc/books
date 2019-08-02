2583ba67bdda5ef3691bee685cd8d527fedd2bb4
Hector Rincon <hecerinc@hotmail.com>
2019-08-01 22:30:31 -0500

    Add nav file

---
+++ src/nav.php
@@ -0,0 +1,18 @@
+<aside class="main-sidebar collapsed">
+	<a class="menu-toggle" href="#">
+		<img src="img/hamburger.svg" alt="Toggle menu">
+	</a>
+	<nav class="side-menu collapsed">
+		<ul>
+			<li class="nav-link">
+				<a class="link-text" href="/"><span class="ico book"></span>Books</a>
+				<ul class="hidden">
+					<li class="nav-link"><a href="addnew.php">+ Add new</a></li>
+				</ul>
+			</li>
+			<li class="nav-link">
+				<a class="link-text" href="lists.php"><span class="ico lists"></span>Lists</a>
+			</li>
+		</ul>
+	</nav>
+</aside>
