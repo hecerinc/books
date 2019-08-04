56e54031a5f9383201be1a82d757889533633817
Hector Rincon <hecerinc@hotmail.com>
2019-08-04 17:19:46 -0500

    Pull out grid into its own file

---
+++ src/_bookgrid.php
@@ -0,0 +1,14 @@
+<?php foreach($books as $book): ?>
+	<?php $url = 'file:///'.$book['path'] . '/' . $book['filename']; ?>
+	<div class="test">
+		<article class="Book">
+			<a class="coverimg" href="<?= $url ?>" target="_blank">
+				<img src="covers/<?= $book['coverimg'] ?>" alt="<?= $book['name'] ?>">
+			</a>
+			<div class="info">
+				<p><a href="<?= $url ?>" target="_blank" class="name"><?= $book['name'] ?></a></p>
+				<p><a href="<?= $url ?>" target="_blank" class="authors"><?= $book['authors'] ?></a></p>
+			</div>
+		</article>
+	</div>
+<?php endforeach; ?>
