d16a954a6ba1b6ed62c9f04e4388567151d28b0e
Hector Rincon <hecerinc@hotmail.com>
2019-08-04 22:42:01 -0500

    Enable editing of collections

--- src/editlist.php
+++ src/editlist.php
@@ -5,12 +5,12 @@
 <?php if(isset($_SESSION['listsave_success'])): ?>
 	<p style="color: green;">Successfully saved collection.</p>
 <?php unset($_SESSION['listsave_success']); endif; ?>
-
-<h1>Edit collection</h1>
-
 <?php
 	$is_edit = true;
-	$list_id = $_GET['id'];
+	$list_id = isset($_GET['id']) ? $_GET['id'] : NULL;
 ?>
+<a href="lists.php?id=<?= $list_id ?>"><small>view collection</small></a>
+<h1>Edit collection</h1>
+
 <?php require_once '_listform.php'; ?>
 <?php require_once 'footer.php'; ?>
--- src/process_lists.php
+++ src/process_lists.php
@@ -14,7 +14,7 @@ if($_SERVER['REQUEST_METHOD'] != 'POST') {
 // SQL stmts
 $_INSERTLIST = "INSERT INTO collections(name, icon) VALUES(?, ?)";
 $_INSERT_WITH_PARENT = "INSERT INTO collections(name, icon, parent_id) VALUES(?, ?, ?)";
-
+$_DELETEASSOC = 'DELETE FROM books_collections WHERE collection_id = ?';
 
 // Check what the form sent and store them
 if(isset($_POST['books'])) {
@@ -23,7 +23,6 @@ if(isset($_POST['books'])) {
 $name = $_POST['name'];
 $parent_cat = $_POST['parent'];
 $icon = $_FILES['icon'];
-
 
 // Try to upload the file if exists
 if(is_uploaded_file($_FILES['icon']['tmp_name'])) {
@@ -34,6 +33,12 @@ if(is_uploaded_file($_FILES['icon']['tmp_name'])) {
 		http_response_code(500);
 		die('Could not move image');
 	}
+}
+
+// Check if we're editing instead of inserting a new one.
+$is_edit = isset($_POST['edit_id']);
+if($is_edit) {
+	return is_edit();
 }
 
 // Decide which insert based on whether this is a child collection or not
@@ -57,40 +62,108 @@ $collection_id = $stmt->insert_id;
 $stmt->close();
 
 
-
-// Associate books (if any)
-$_MULTIINSERT = 'INSERT INTO books_collections(book_id, collection_id) VALUES';
-
-// --- First we have to build the query so that we can execute as prepared statements
-// --- into a single INSERT SQL statement (dramatically faster)
-
-$ids = [];
-$len = count($books);
-foreach ($books as $key => $book) {
-	$_MULTIINSERT .= ' (?, ?)';
-	if($key != $len-1) {
-		$_MULTIINSERT .= ',';
-	}
-	$ids[] = $book;
-	$ids[] = $collection_id;
-}
-
-// Prepare & bind
-$stmt = $db->conn->prepare($_MULTIINSERT);
-if($stmt === false) {
-	die($db->conn->error);
-}
-
-$type_str = str_repeat('ii', count($books));
-$stmt->bind_param($type_str, ...$ids);
+// Associate books
+$stmt = generate_book_stmt($books, $collection_id);
 
 if(!$stmt->execute()) {
 	$stmt->close();
 	http_response_code(500);
 	die('Something went wrong when saving the list');
 }
+$stmt->close();
 
 session_start();
 $_SESSION['listsave_success'] = 'success';
 header('Location: addlist.php');
 
+
+
+function generate_book_stmt($books, $collection_id) {
+	global $db;
+
+	// Associate books (if any)
+	$_MULTIINSERT = 'INSERT INTO books_collections(book_id, collection_id) VALUES';
+
+	// --- First we have to build the query so that we can execute as prepared statements
+	// --- into a single INSERT SQL statement (dramatically faster)
+
+	$ids = [];
+	$len = count($books);
+	foreach ($books as $key => $book) {
+		$_MULTIINSERT .= ' (?, ?)';
+		if($key != $len-1) {
+			$_MULTIINSERT .= ',';
+		}
+		$ids[] = $book;
+		$ids[] = $collection_id;
+	}
+
+	// Prepare & bind
+	$stmt = $db->conn->prepare($_MULTIINSERT);
+	if($stmt === false) {
+		die($db->conn->error);
+	}
+
+	$type_str = str_repeat('ii', count($books));
+	$stmt->bind_param($type_str, ...$ids);
+
+	return $stmt;
+}
+
+
+function is_edit() {
+	global $db;
+	global $new_name;
+	global $name;
+	global $parent_cat;
+	global $books;
+	global $_DELETEASSOC;
+
+	$parent_cat = $parent_cat === '' ? NULL : $parent_cat;
+	$_UPDATE = 'UPDATE collections SET name = ?, parent_id = ?, modified = NOW()';
+	if(isset($new_name)) { // do we have an image?
+		$_UPDATE .= ', icon = ?';
+	}
+	$_UPDATE .= ' WHERE id = ?';
+
+	var_dump($_UPDATE);
+	$update_stmt = $db->conn->prepare($_UPDATE);
+	$edit_id = $_POST['edit_id'];
+	if(isset($new_name)) {
+		$update_stmt->bind_param('sisi', $name, $parent_cat, $new_name, $edit_id);
+	}
+	else {
+		$update_stmt->bind_param('sii', $name, $parent_cat, $edit_id);
+	}
+	if(!$update_stmt->execute()) {
+		// $update_stmt->close();
+		http_response_code(500);
+		die("Failed to update the entry. Please try again. <br>". $update_stmt->error);
+	}
+	$update_stmt->close();
+
+	// Delete and reinsert all associated books
+	$delete_stmt = $db->conn->prepare($_DELETEASSOC);
+	$delete_stmt->bind_param('i', $edit_id);
+	if(!$delete_stmt->execute()) {
+		$delete_stmt->close();
+		http_response_code(500);
+		die('Failed to delete previous associations');
+	}
+	$delete_stmt->close();
+
+	if(isset($books)) {
+		$book_stmt = generate_book_stmt($books, $edit_id);
+		if(!$book_stmt->execute()) {
+			$book_stmt->close();
+			http_response_code(500);
+			die('Failed to associate books.');
+		}
+		$book_stmt->close();
+	}
+
+	session_start();
+	$_SESSION['listsave_success'] = 'success';
+	header('Location: editlist.php?id='.$edit_id);
+
+}
