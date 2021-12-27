353c1890f33357bcf089b6435de500cd605ba5bf
Hector Rincon <hecerinc@hotmail.com>
2021-12-26 21:25:00 -0800

    Add prettier config

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
-const sass = require("gulp-sass")(require("sass"));
-var sourcemaps = require("gulp-sourcemaps");
-var autoprefixer = require("gulp-autoprefixer");
-var notify = require("gulp-notify");
-// var plumber = require('gulp-plumber');
-var php = require("gulp-connect-php");
-var browserSync = require("browser-sync");
-var del = require("del");
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const notify = require('gulp-notify');
+// const plumber = require('gulp-plumber');
+const php = require('gulp-connect-php');
+const browserSync = require('browser-sync');
+const del = require('del');
 
 function myphp(cb) {
-	php.server({ base: "build/", port: 8010, keepalive: true });
+	php.server({ base: 'build/', port: 8010, keepalive: true });
 	cb();
 }
 
@@ -18,38 +18,34 @@ function styles() {
 	// Compiles CSS
 	return (
 		gulp
-			.src("./src/css/**/*.scss")
+			.src('./src/css/**/*.scss')
 			// .pipe(plumber())
 			.pipe(sourcemaps.init())
-			.pipe(
-				sass.sync({ outputStyle: "expanded" }).on("error", handleErrors)
-			)
+			.pipe(sass.sync({ outputStyle: 'expanded' }).on('error', handleErrors))
 			.pipe(autoprefixer())
 			.pipe(sourcemaps.write())
-			.pipe(gulp.dest("build/css/"))
+			.pipe(gulp.dest('build/css/'))
 			.pipe(browserSync.reload({ stream: true }))
 	);
 }
 
 function images() {
-	return gulp.src("./src/img/**").pipe(gulp.dest("./build/img/"));
+	return gulp.src('./src/img/**').pipe(gulp.dest('./build/img/'));
 }
 function covers() {
-	return gulp.src("./src/covers/**").pipe(gulp.dest("./build/covers/"));
+	return gulp.src('./src/covers/**').pipe(gulp.dest('./build/covers/'));
 }
 
 function staticss() {
-	return gulp.src("./src/css/*.css").pipe(gulp.dest("./build/css/"));
+	return gulp.src('./src/css/*.css').pipe(gulp.dest('./build/css/'));
 }
 
 function js() {
-	return gulp.src("./src/js/**").pipe(gulp.dest("./build/js/"));
+	return gulp.src('./src/js/**').pipe(gulp.dest('./build/js/'));
 }
 
 function html() {
-	return gulp
-		.src(["./src/*.html", "./src/**/*.php"])
-		.pipe(gulp.dest("./build/"));
+	return gulp.src(['./src/*.html', './src/**/*.php']).pipe(gulp.dest('./build/'));
 }
 
 function handleErrors(e) {
@@ -59,15 +55,15 @@ function handleErrors(e) {
 
 	notify
 		.onError({
-			title: "Compile Error",
-			message: "<%= error.message %>",
+			title: 'Compile Error',
+			message: '<%= error.message %>',
 		})
 		.apply(this, args);
-	this.emit("end"); // Keep gulp from hanging on this task
+	this.emit('end'); // Keep gulp from hanging on this task
 }
 
 function clean() {
-	return del(["css", "img", "js", "./*.html", "./*.php", "build"]);
+	return del(['css', 'img', 'js', './*.html', './*.php', 'build']);
 }
 
 /*
@@ -76,7 +72,7 @@ function clean() {
 function bsync(cb) {
 	browserSync.init({
 		// we need to disable clicks and forms for when we test multiple rooms
-		proxy: "127.0.0.1:8010",
+		proxy: '127.0.0.1:8010',
 		// server: {
 		// 	baseDir: './', // ./build/
 		// },
@@ -88,30 +84,20 @@ function bsync(cb) {
 	cb();
 }
 function watch() {
-	gulp.watch("src/css/*.scss", gulp.series(styles));
-	gulp.watch("src/css/*.css", gulp.series(staticss));
-	gulp.watch("src/img/*", gulp.series(images));
-	gulp.watch("src/covers/*", gulp.series(covers));
-	gulp.watch("src/js/*", gulp.series(js));
-	gulp.watch("src/*.html", gulp.series(html));
-	gulp.watch("src/**/*.php", gulp.series(html));
-	gulp.watch("./build/*.php").on("change", browserSync.reload);
-	gulp.watch("./build/js/*.js").on("change", browserSync.reload);
-	gulp.watch("./build/img/**").on("change", browserSync.reload);
-	gulp.watch("./build/covers/**").on("change", browserSync.reload);
-	gulp.watch("./build/*.html").on("change", browserSync.reload);
+	gulp.watch('src/css/*.scss', gulp.series(styles));
+	gulp.watch('src/css/*.css', gulp.series(staticss));
+	gulp.watch('src/img/*', gulp.series(images));
+	gulp.watch('src/covers/*', gulp.series(covers));
+	gulp.watch('src/js/*', gulp.series(js));
+	gulp.watch('src/*.html', gulp.series(html));
+	gulp.watch('src/**/*.php', gulp.series(html));
+	gulp.watch('./build/*.php').on('change', browserSync.reload);
+	gulp.watch('./build/js/*.js').on('change', browserSync.reload);
+	gulp.watch('./build/img/**').on('change', browserSync.reload);
+	gulp.watch('./build/covers/**').on('change', browserSync.reload);
+	gulp.watch('./build/*.html').on('change', browserSync.reload);
 }
-var build = gulp.series(
-	styles,
-	staticss,
-	images,
-	covers,
-	html,
-	js,
-	myphp,
-	bsync,
-	watch
-);
+var build = gulp.series(styles, staticss, images, covers, html, js, myphp, bsync, watch);
 
 exports.myphp = myphp;
 exports.clean = clean;
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'avoid',
+};
